8a6861d4925dc23f3e708a855bd0e4ce9defc8c4
Eder Nuñez <santieder16@gmail.com>
2025-03-28 21:08:20 -0500

    implementing expanding tape

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,65 @@
 use std::fmt::Display;
 
 #[derive(Default)]
+struct Tape {
+    data: Vec<bool>,
+    count: usize,
+    capacity: usize,
+}
+
+impl Tape {
+    fn tape_randomize(&mut self) {
+        self.data.iter_mut().for_each(|b| *b = rand::random());
+    }
+}
+
+#[derive(Default)]
 struct Machine {
-    size: usize,
-    data: Box<[bool]>,
+    tape: Tape,
     head: usize,
 }
 
 impl Display for Machine {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut s = self
+            .tape
             .data
             .iter()
-            .take(self.size)
+            .take(self.tape.count)
             .fold(format!("head: {}, ", self.head), |acc, b| {
                 format!("{}{} -> ", acc, u8::from(*b))
             });
-        s.push_str(&format!("{}", u8::from(self.data[self.head])));
+        s.remove(s.len() - 2);
+        s.remove(s.len() - 2);
         write!(f, "{s}")
     }
 }
 
 impl Machine {
-    fn machine_randomize(&mut self) {
-        self.data.iter_mut().for_each(|b| *b = rand::random());
-    }
-
     fn machine_execute(&mut self, inst: &Instruction, inst_count: usize) -> usize {
-        if self.head >= self.size {
-            return inst_count;
+        if self.head >= self.tape.capacity {
+            self.tape.data.resize(self.tape.capacity * 2, false);
+            self.tape.capacity *= 2;
         }
-        if self.data[self.head] == inst.expected {
-            self.data[self.head] = inst.yes.write;
+        if self.tape.data[self.head] == inst.expected {
+            self.tape.data[self.head] = inst.yes.write;
             if self.head == 0 && (inst.yes.dir as i8) < 0 {
                 return inst_count;
             }
             self.head = (self.head as isize + inst.yes.dir as isize) as usize;
+            if self.head > self.tape.count {
+                self.tape.count = self.head;
+            }
             return inst.yes.next;
         }
-        self.data[self.head] = inst.no.write;
+        self.tape.data[self.head] = inst.no.write;
         if self.head == 0 && (inst.no.dir as i8) < 0 {
             return inst_count;
         }
         self.head = (self.head as isize + inst.no.dir as isize) as usize;
+        if self.head > self.tape.count {
+            self.tape.count = self.head;
+        }
         inst.no.next
     }
 }
@@ -53,7 +70,7 @@ enum Direction {
     Stay = 0,
     Right = 1,
 }
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 struct State {
     write: bool,
     dir: Direction,
@@ -84,18 +101,21 @@ struct Program {
     inst_count: usize,
     cur: usize,
 }
+
 fn main() {
-    let argv = std::env::args().collect::<Vec<_>>();
+    let argv = std::env::args().collect::<Vec<_>>().into_boxed_slice();
     let mut program = Program::default();
     if argv.len() == 1 {
-        program.machine.size = 5;
-        program.machine.data = vec![false; program.machine.size].into_boxed_slice();
-        program.machine.machine_randomize();
+        program.machine.tape.capacity = 3;
+        program.machine.tape.count = program.machine.tape.capacity;
+        program.machine.tape.data = vec![false; program.machine.tape.capacity];
+        program.machine.tape.tape_randomize();
     } else {
-        program.machine.size = argv[1].len();
-        program.machine.data = vec![false; program.machine.size].into_boxed_slice();
-        (0..program.machine.size)
-            .for_each(|i| program.machine.data[i] = argv[1].as_bytes()[i] == b'1');
+        program.machine.tape.capacity = argv[1].len();
+        program.machine.tape.count = program.machine.tape.capacity;
+        program.machine.tape.data = vec![false; program.machine.tape.capacity];
+        (0..program.machine.tape.capacity)
+            .for_each(|i| program.machine.tape.data[i] = argv[1].as_bytes()[i] == b'1');
     }
     program.insts = Box::new([Instruction::new(
         false,
